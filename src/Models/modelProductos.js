08c2b86010f5eb80bb4c5bb23c438bf4d2f6fe8a
fmarcos38 <fmarcos_23@hotmail.com>
2024-08-30 11:50:47 -0300

    se agrego precio y kg ofertas

--- src/Models/modelProductos.js
+++ src/Models/modelProductos.js
@@ -4,7 +4,9 @@ const ProductoSchema = Schema({
     nombre: {type: String, required: true, unique: true},
     unidadMedida: {type: String, },
     precioKg: {type: Number, required: true},
+    precioKgContado: {type: Number, required: true},
     envase: {type: Number, required: true },
+    envaseEspecial: {type: Number, required: true },
     costo: {type: Number, required: true },
     posicionLista: {type: Number, required: true },
 });
